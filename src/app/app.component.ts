f24a7a1b5c5dc2f602f1197c7d95f31f0787c6b9
nabendu82 <nabendu.biswas@gmail.com>
2019-01-15 21:24:47 +0530

    ComponentBindingv1.0

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+  onIntervalFired(num: number) {
+    if(num % 2 === 0) {
+      this.evenNumbers.push(num);
+    } else {
+      this.oddNumbers.push(num);
+    }
+  }
+}
